f3ee4116b55d7681c2eaf725f4b5087e9deafc36
Hugo <hquijada@edesarrollos.com>
2019-10-11 15:51:23 -0700

    Update FlutterConektaPlugin.m

--- ios/Classes/FlutterConektaPlugin.m
+++ ios/Classes/FlutterConektaPlugin.m
@@ -53,6 +53,7 @@
             NSString* id = [data objectForKey:@"id"];
             if( ( [id isEqual:[NSNull null]] ) || ( [id length] == 0 ) ) {
                 result([FlutterError errorWithCode:@"ERROR_UNABLE_TO_TOKENIZE" message:[data objectForKey:@"message"] details:nil]);
+                return;
             }
             result(id);
         } andError:^(NSError* error) {
